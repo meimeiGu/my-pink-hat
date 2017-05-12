c75e711ebc4bc41a3c17b0495e953a990af72423
cqjd rjzx <cqjd rjzx>
2017-05-12 17:56:59 +0800

    h

---
+++ src/index/MainContent.js
@@ -0,0 +1,27 @@
+/**
+ * Created by wmm on 2017/5/11.
+ */
+import React from 'react'
+import '../../styles/index.css'
+import Sliding from './Sliding'
+import MainView from './MainView'
+
+
+
+class MainContent extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <div id="mainIndex">
+                <div id="indexRecommend">
+                <Sliding/>
+                <MainView/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MainContent
