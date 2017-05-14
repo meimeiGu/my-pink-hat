6a6e40e81f80664fd0365633329cbd64b09f0685
wangmm <ermei.Gu@gmail.com>
2017-05-14 16:20:26 +0800

    h

--- src/index/Sliding.js
+++ src/index/Sliding.js
@@ -5,30 +5,46 @@ import React from 'react'
 import '../../styles/global.css'
 import '../../styles/index.css'
 
+/*class SlidingJS extends React.Component{
+    autoMove = () =>{
+
+
+    }
+
+}*/
 class Sliding extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {}
     }
+/*    autoPlay = () =>{
+
+
+    }
+    componentDidMount = () =>{
+        this.autoPlay();
+    }*/
+
     render(){
         return(
             <div id="sliding">
                 <ul className="slider-outer">
-                    {/*<li className="slider-html">
+                    <li className="slider-html" data-index = {1}>
                         <img src="../../images/slider/a.jpeg"/>
                     </li>
-                    <li className="slider-html ">
+                    <li className="slider-html slider-prev" data-index = {2}>
                         <img src="../../images/slider/b.jpeg"/>
                     </li>
-                    <li className="slider-html ">
+                    <li className="slider-html slider-active" data-index = {3}>
                         <img src="../../images/slider/d.jpeg"/>
                     </li>
-                    <li className="slider-html">
+                    <li className="slider-html slider-next" data-index = {4}>
                         <img src="../../images/slider/f.jpeg"/>
                     </li>
-                    <li className="slider-html">
+                    <li className="slider-html" data-index = {5}>
                         <img src="../../images/slider/g.jpeg"/>
-                    </li>*/}
-                    <li className="slider-html slider-active">
+                    </li>
+                    <li className="slider-html" data-index = {6}>
                         <img src="../../images/slider/h.jpeg"/>
                     </li>
                 </ul>
