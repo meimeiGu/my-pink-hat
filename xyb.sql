afdab3ac5a183a59487759e270b3403ba4061a1a
xbsygzs <285716305@qq.com>
2017-05-16 09:01:34 +0800

    edit xyb.sql

--- xyb.sql
+++ xyb.sql
@@ -20,24 +20,24 @@ CREATE TABLE `xyb_user` (
 DROP TABLE IF EXISTS `xyb_gbgoods`;
 CREATE TABLE `xyb_gbgoods` (
   `gbgoods_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '团购商品编号',
-  `gbgoods_name` varchar(20) NULL COMMENT '团购商品名称',  
-  `gbgoods_originprice` decimal(10,2)  NULL  COMMENT '团购商品原价', 
-  `gbgoods_price` decimal(10,2) NULL COMMENT '团购商品价格',
-  `gbgoods_gbprice` decimal(10,2) NULL COMMENT '团购商品团购价',
-  `gbgoods_pic` varchar(50) NULL COMMENT '轮播图片',
-  `gbgoods_pnum` tinyint(4) NULL COMMENT '规定团购人数',
-  `gbgoods_tnum` int(11) NULL COMMENT '已团人数',
-  `gbgoods_num` int(30) NULL COMMENT '数量',
-  `gbgoods_details` mediumtext NULL COMMENT '商品详情',
-  `gbgoods_picdetails` mediumtext NULL COMMENT '商品图片详情',  
+  `gbgoods_name` varchar(20) DEFAULT NULL COMMENT '团购商品名称',  
+  `gbgoods_originprice` decimal(10,2) DEFAULT NULL  COMMENT '团购商品原价', 
+  `gbgoods_price` decimal(10,2) DEFAULT NULL COMMENT '团购商品价格',
+  `gbgoods_gbprice` decimal(10,2) DEFAULT NULL COMMENT '团购商品团购价',
+  `gbgoods_pic` varchar(50) DEFAULT NULL COMMENT '轮播图片',
+  `gbgoods_pnum` tinyint(4) DEFAULT NULL COMMENT '规定团购人数',
+  `gbgoods_gbnum` int(11) DEFAULT NULL COMMENT '已团人数',
+  `gbgoods_num` int(30) DEFAULT NULL COMMENT '剩余数量',
+  `gbgoods_details` mediumtext DEFAULT NULL COMMENT '商品详情',
+  `gbgoods_picdetails` mediumtext DEFAULT NULL COMMENT '商品图片详情',  
   PRIMARY KEY (`gbgoods_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `xyb_gbrull`;
 CREATE TABLE `xyb_gbrull` ( 
-  `gbrull_id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT '服务编号',
-  `gbrull_title` varchar(30) NOT NULL COMMENT '服务标题',
-  `gbrull_comment` varchar(50) NOT NULL COMMENT '服务解释',
+  `gbrull_id` smallint(6) DEFAULT NULL AUTO_INCREMENT COMMENT '服务编号',
+  `gbrull_title` varchar(30) DEFAULT NULL COMMENT '服务标题',
+  `gbrull_comment` varchar(50) DEFAULT NULL COMMENT '服务解释',
   PRIMARY KEY (`gbrull_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -46,22 +46,22 @@ CREATE TABLE `xyb_gborder` (
   `gborder_id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT '总订单编号',
   `userId` smallint(6) DEFAULT NULL COMMENT '用户编号',
   `gborder_state` tinyint(4) DEFAULT NULL COMMENT '总订单状态',
-  `order_time` datetime DEFAULT NULL COMMENT '总订单时间',
-  `order_price` decimal(8,1) DEFAULT NULL COMMENT '订单总价格',
-  `order_phone` varchar(12) DEFAULT NULL COMMENT '订单联系人号码',
+  `gborder_time` datetime DEFAULT NULL COMMENT '总订单时间',
+  `gborder_price` decimal(8,1) DEFAULT NULL COMMENT '订单总价格',
+  `gborder_phone` varchar(12) DEFAULT NULL COMMENT '订单联系人号码',
   PRIMARY KEY (`gborder_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `xyb_gbsingleorder`;
 CREATE TABLE `xyb_gbsingleorder` (
-  `gbsingleorder_id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT '团购商品单条商品编号',
+  `gbsingleorder_id` smallint(6) NOT NULL AUTO_INCREMENT COMMENT '订单单条商品编号',
   `gborder_id` smallint(6) DEFAULT NULL COMMENT '总订单编号',
   `userId` int(11) DEFAULT NULL COMMENT '用户编号',
   `gbgoods_id` int(11) DEFAULT NULL COMMENT '团购商品编号',
-  `gbgoodsPrice` decimal(5,1) DEFAULT NULL COMMENT '单条商品价格',
+  `gbsingleorder_price` decimal(5,1) DEFAULT NULL COMMENT '单条商品价格',
   `gbsingleorder_num` smallint(6) DEFAULT NULL COMMENT '单条商品数量',
   `gbsingleorder_state` smallint(6) DEFAULT NULL COMMENT '单挑商品状态',
-  PRIMARY KEY (`orderSid`)
+  PRIMARY KEY (`gbsingleorder_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
