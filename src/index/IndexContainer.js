fa3c4fadf79c18024892a762845580d6b9636d58
xbsygzs <285716305@qq.com>
2017-05-22 12:39:23 +0800

    fas

---
+++ src/index/IndexContainer.js
@@ -0,0 +1,23 @@
+/**
+ * Created by wmm on 2017/5/11.
+ */
+import React from 'react'
+import Navigation from './Navigation'
+import MainContent from './MainContent'
+import '../../styles/global.css'
+
+class IndexContainer extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <div id="index-content">
+                <Navigation />
+                <MainContent />
+            </div>
+        )
+    }
+}
+
+export default IndexContainer
