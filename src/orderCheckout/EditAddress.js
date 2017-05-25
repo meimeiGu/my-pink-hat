aab850d7a8944ea6b983b325539c37f738589dbf
wangmm <ermei.Gu@gmail.com>
2017-05-25 22:54:12 +0800

    525

--- src/orderCheckout/EditAddress.js
+++ src/orderCheckout/EditAddress.js
@@ -9,13 +9,17 @@ class EditAddress extends React.Component{
         this.state = {name:'',phone:'',ok:false}
         this.saveAddress = this.saveAddress.bind(this);
     }
+
     saveAddress() {
         console.log(document.getElementById('name').checkValidity())
+        console.log(document.getElementById('mobile').checkValidity()+'hh')
     if(document.getElementById('name').checkValidity() && document.getElementById('mobile').checkValidity()){
-        this.setState({ok:true})
+
+        this.props.open(false)
     }else {
         alert('shuru')
     }
+
 
     }
     render(){
@@ -27,7 +31,7 @@ class EditAddress extends React.Component{
                     <input className="add-name" id="name" placeholder="名字" type="text" required />
                         <input className="add-mobile" id="mobile" placeholder="电话" type="tel" required />
                 </div>
-                <div className="add-save" onClick={()=>{this.saveAddress;this.state.ok?this.props.open(false):null}}>保存</div>
+                <div className="add-save" onClick={()=>{this.saveAddress()}}>保存</div>
                 <div className="add-close" onClick={()=>{this.props.open(false)}}>
                     <i className="iconfont icon-cha"></i>
 
